17eec596a7f1b0f1b9b7b70d3314826070cfd64a
Jason Smith <jason.inet@gmail.com>
2023-03-07 19:04:17 -0800

    Re-calibrate wheels

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -40,25 +40,25 @@ public final class Constants {
     public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 5;
     public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 11;
     // public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(230.416);
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(230.625);
 
     public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 6;
     public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 2;
     public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 10;
     // public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(188.);
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(196.875);
 
     public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 4;
     public static final int BACK_LEFT_MODULE_STEER_MOTOR = 7;
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 13;
     // public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(258.2);
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(259.8);
 
     public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 8;
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 3;
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 12;
     // public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(104.0);
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(104.15);
 
     public static final int ARM_MOTOR_ID = 9;
     public static final int WRIST_SERVO_ID = 0;
